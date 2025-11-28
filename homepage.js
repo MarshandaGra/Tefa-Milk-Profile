32b3f6410e4fe8fbc589a9bd1912ec095baef19d
Marsh <113867663+MarshandaGra@users.noreply.github.com>
2025-11-29 01:42:06 +0700

    perbaikan dari marsha, janlup pull dulu guiss

--- homepage.js
+++
@@ -1,9 +0,0 @@
-window.addEventListener("scroll", function () {
-    const navbar = document.querySelector(".custom-navbar");
-
-    if (window.scrollY > 10) {
-        navbar.classList.add("scrolled");
-    } else {
-        navbar.classList.remove("scrolled");
-    }
-});
