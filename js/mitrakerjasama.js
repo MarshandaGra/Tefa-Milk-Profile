35fab1a0b8f32737ae0a97ff654bb01e6592b794
Marsh <113867663+MarshandaGra@users.noreply.github.com>
2025-12-03 11:10:54 +0700

    habis presentasi

--- js/mitrakerjasama.js
+++ js/mitrakerjasama.js
@@ -1,23 +1,3 @@
-document.querySelectorAll(".faq-question").forEach(function(button) {
-    button.addEventListener("click", function() {
-
-        const faqItem = button.parentElement;
-        const isActive = faqItem.classList.contains("active");
-
-        // Tutup semua item lain
-        document.querySelectorAll(".faq-item").forEach(function(item) {
-            item.classList.remove("active");
-            item.querySelector(".icon").textContent = "+";
-        });
-
-        // Jika sebelumnya tidak aktif → buka
-        if (!isActive) {
-            faqItem.classList.add("active");
-            faqItem.querySelector(".icon").textContent = "−";
-        }
-    });
-});
-
 document.addEventListener('DOMContentLoaded', function () {
     const btn = document.getElementById('waSubmit');
     if (!btn) return;
